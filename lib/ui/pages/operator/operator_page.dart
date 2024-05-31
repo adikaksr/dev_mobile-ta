2bc61e3f4e45f3c06e83b3db887a022c4ff4266f
Teuku Zafran Elrifqi <t.zafran@mhs.unsyiah.ac.id>
2024-05-31 13:52:17 +0700

    Update operator_page.dart

--- lib/ui/pages/operator/operator_page.dart
+++ lib/ui/pages/operator/operator_page.dart
@@ -8,7 +8,108 @@ class OperatorPage extends StatelessWidget {
         title: Text('Operator Page'),
       ),
       body: Center(
-        child: Text('Welcome, Operator!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Welcome, Operator!'),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: ElevatedButton(
+                child: Text('Create Mahasiswa Account'),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      TextEditingController nipController =
+                          TextEditingController();
+                      return AlertDialog(
+                        title: Text('Create Mahasiswa Account'),
+                        content: Column(
+                          children: <Widget>[
+                            Text('Please enter the NIP:'),
+                            TextField(
+                              controller: nipController,
+                              decoration: InputDecoration(
+                                hintText: 'NIP',
+                              ),
+                            ),
+                          ],
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          TextButton(
+                            child: Text('Submit'),
+                            onPressed: () {
+                              String nip = nipController.text;
+                              Navigator.of(context).pop();
+                              // Pass the NIP to the next page
+                              Navigator.of(context).pushNamed(
+                                  '/create-mahasiswa-account',
+                                  arguments: nip);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: ElevatedButton(
+                child: Text('Create Mahasiswa Account'),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      TextEditingController nipController =
+                          TextEditingController();
+                      return AlertDialog(
+                        title: Text('Create Mahasiswa Account'),
+                        content: Column(
+                          children: <Widget>[
+                            Text('Please enter the NIP:'),
+                            TextField(
+                              controller: nipController,
+                              decoration: InputDecoration(
+                                hintText: 'NIP',
+                              ),
+                            ),
+                          ],
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          TextButton(
+                            child: Text('Submit'),
+                            onPressed: () {
+                              String nip = nipController.text;
+                              Navigator.of(context).pop();
+                              // Pass the NIP to the next page
+                              Navigator.of(context).pushNamed(
+                                  '/create-mahasiswa-account',
+                                  arguments: nip);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
